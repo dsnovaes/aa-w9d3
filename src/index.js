e3559bb4b65c021915a52438028c4dc76d4df0de
Diego Novaes <dsnovaes1@gmail.com>
2022-11-30 16:56:03 -0800

    tic tac toe done

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
   // Your code here
   const game = new Game()
   const grid = document.querySelector("figure.ttt")
-  
   const view = new View(game,grid)
   window.view = view
 
-    const positions = document.getElementById("grid")
-    positions.addEventListener("click",(e) => view.handleClick(e))
+  const positions = document.getElementById("grid")
+  positions.addEventListener("click",(e) => view.handleClick(e))
   
 });
--- src/ttt-view.js
+++ src/ttt-view.js
@@ -18,26 +18,42 @@ class View {
       for (let col = 0; col < 3; col++) {
    
         let li = document.createElement('li');
-        li.setAttribute('data-position', `[${row},${col}]`);
-
+        li.setAttribute('data-position', `[${row},${col}]`); 
+        li.setAttribute('class', 'available');
         ul.appendChild(li);
     }}
   }
   
   bindEvents() {
-    // const positions = document.getElementsByTagName("ul")
-    // positions.addEventListener("click",(e) => this.handleClick(e))
     
   }
 
   handleClick(e) {
-    //this.game.playMove()
-    console.log(e)
+    if (!this.game.board.isOver()) {
+      let index1 = parseInt(e.target.attributes[0].value[1])
+      let index2 = parseInt(e.target.attributes[0].value[3])
+      let toArray = [index1,index2]
+      if (this.game.board.isEmptyPos(toArray)) {
+        this.makeMove(toArray)
+        e.target.innerHTML = this.game.currentPlayer;
+        e.target.classList.add(this.game.currentPlayer)
+        e.target.classList.remove('available')
+      } else {
+        alert("invalid move")
+      }
+    } else {
+      console.log("game is over, go home")
+    }
     
   }
 
   makeMove(square) {
-
+    this.game.playMove(square)
+    if (this.game.board.winner()) {
+      const ul = document.getElementsByTagName("ul")[0]
+      ul.classList.add('gameOver')
+      alert('game over');
+    }
   }
 
 }
